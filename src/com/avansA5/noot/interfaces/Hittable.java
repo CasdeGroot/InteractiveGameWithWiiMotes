3cf68883a9ff0097764f69b9cfee8c258c03891f
Michael <manet@avans.nl>
2016-04-30 00:05:27 +0200

    Added Pickup abstract class

--- src/com/avansA5/noot/interfaces/Hittable.java
+++ src/com/avansA5/noot/interfaces/Hittable.java
@@ -1,11 +1,9 @@
 package com.avansA5.noot.interfaces;
 
-import com.avansA5.noot.objects.GameObject;
-
 /**
  * Interface for objects that can receive damage
  */
-public interface Hittable
+interface Hittable
 {
     void onHit(Hittable sender);
 }
