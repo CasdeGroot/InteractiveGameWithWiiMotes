425ad2467d53bdf8b0244129a6056af1d7e9205c
Michael <manet@avans.nl>
2016-05-19 14:23:13 +0200

    Added Tickcount Fixed addWiimoteListener not being static Added a few properties to character

--- src/com/avansA5/noot/objects/Characters/Character.java
+++ src/com/avansA5/noot/objects/Characters/Character.java
@@ -1,15 +1,17 @@
 package com.avansA5.noot.objects.Characters;
 
-import com.avansA5.noot.interfaces.Damaging;
-import com.avansA5.noot.interfaces.Shootable;
+import com.avansA5.noot.interfaces.*;
 import com.avansA5.noot.objects.GameObject;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by Cas on 19/05/2016.
  */
-public abstract class Character extends GameObject implements Shootable, Hittable{
-
-    public Character(){
-
+public abstract class Character extends GameObject implements Shootable, Hittable
+{
+    BufferedImage sprite;
+    public Character()
+    {
+        ControlManager.
     }
 }
