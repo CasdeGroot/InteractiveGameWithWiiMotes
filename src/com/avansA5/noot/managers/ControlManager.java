5c7981b5a690e621a6f6f8dc6423d8d7e1f910f1
Michael <manet@avans.nl>
2016-05-19 11:48:12 +0200

    Wiimotes

--- src/com/avansA5/noot/managers/ControlManager.java
+++ src/com/avansA5/noot/managers/ControlManager.java
@@ -1,8 +1,22 @@
 package com.avansA5.noot.managers;
 
+import wiiusej.WiiUseApiManager;
+import wiiusej.Wiimote;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ControlManager
 {
-    public ControlManager(){
+    public static ArrayList<Wiimote> wiimotes;
+    public static void start()
+    {
+        Wiimote[] _wiimotes = WiiUseApiManager.getWiimotes(1, true);
+        wiimotes = new ArrayList(Arrays.asList(_wiimotes));
 
+        for(int i = 0; i > wiimotes.size(); i++)
+        {
+
+        }
     }
 }
