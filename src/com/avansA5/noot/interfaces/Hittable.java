4a1a97595a92c6a4858e616652f7b4732b93959e
Cas Koopmans <cjwkoopm@avans.nl>
2016-05-19 11:26:28 +0200

    Made hittable public and controlmanager constructor

--- src/com/avansA5/noot/interfaces/Hittable.java
+++ src/com/avansA5/noot/interfaces/Hittable.java
@@ -3,7 +3,7 @@ package com.avansA5.noot.interfaces;
 /**
  * Interface for objects that can receive damage
  */
-interface Hittable
+public interface Hittable
 {
     void onHit(Hittable sender);
 }
--- src/com/avansA5/noot/managers/ControlManager.java
+++ src/com/avansA5/noot/managers/ControlManager.java
@@ -2,5 +2,7 @@ package com.avansA5.noot.managers;
 
 public class ControlManager
 {
+    public ControlManager(){
 
+    }
 }
