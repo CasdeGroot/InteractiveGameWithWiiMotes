2eaed6c9f53d1079dee962dbe9e65aa83baa80d5
Cas Koopmans <cjwkoopm@avans.nl>
2016-05-19 14:52:41 +0200

    Player added

---
+++ src/com/avansA5/noot/objects/characters/Player.java
@@ -0,0 +1,29 @@
+package com.avansA5.noot.objects.characters;
+
+import com.avansA5.noot.interfaces.Hittable;
+
+import java.awt.*;
+
+/**
+ * Created by Cas on 19/05/2016.
+ */
+public class Player extends Character{
+    public Player()
+    {
+
+    }
+    @Override
+    public void draw(Graphics2D g2) {
+
+    }
+
+    @Override
+    public void onHit(Hittable sender) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
