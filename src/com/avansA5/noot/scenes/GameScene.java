064cca9c32d9669c54227152050d8942d631116b
Cas Koopmans <cjwkoopm@avans.nl>
2016-05-19 15:24:29 +0200

    added GameScene

---
+++ src/com/avansA5/noot/scenes/GameScene.java
@@ -0,0 +1,16 @@
+package com.avansA5.noot.scenes;
+
+/**
+ * Created by Cas on 19/05/2016.
+ */
+public class GameScene extends Scene {
+    @Override
+    public void load() {
+
+    }
+
+    @Override
+    public void unload() {
+
+    }
+}
