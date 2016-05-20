9eb3fa683b867648429d15a03c54434e28cdbfb8
Cas Koopmans <cjwkoopm@avans.nl>
2016-05-20 12:39:19 +0200

    Moving character, aiming and full screen done

--- src/com/avansA5/noot/objects/CrossHair.java
+++ src/com/avansA5/noot/objects/CrossHair.java
@@ -1,5 +1,6 @@
 package com.avansA5.noot.objects;
 
+import com.avansA5.noot.managers.ControlManager;
 import wiiusej.wiiusejevents.physicalevents.ExpansionEvent;
 import wiiusej.wiiusejevents.physicalevents.IREvent;
 import wiiusej.wiiusejevents.physicalevents.MotionSensingEvent;
@@ -9,6 +10,7 @@ import wiiusej.wiiusejevents.wiiuseapievents.*;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.Arc2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -18,6 +20,7 @@ import java.io.IOException;
  */
 public class CrossHair extends GameObject implements WiimoteListener {
 
+    int x, y;
     BufferedImage image;
     String imageString;
 
@@ -35,7 +38,7 @@ public class CrossHair extends GameObject implements WiimoteListener {
 
     @Override
     public void draw(Graphics2D g2) {
-        g2.drawImage(image, 400, 400, image.getWidth()/5, image.getHeight()/5, null);
+        g2.drawImage(image, x, y, image.getWidth()/5, image.getHeight()/5, null);
     }
 
     @Override
@@ -51,6 +54,8 @@ public class CrossHair extends GameObject implements WiimoteListener {
     @Override
     public void onIrEvent(IREvent irEvent) {
 
+        x = irEvent.getAx() * 2;
+        y = irEvent.getAy() * 2;
     }
 
     @Override
